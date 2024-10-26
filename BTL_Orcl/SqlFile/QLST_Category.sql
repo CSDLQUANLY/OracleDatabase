3300c250969826686538c523daed3254333f5ff6
quangnv04 <ngovanquang2601@gmail.com>
2024-10-26 21:07:45 +0700

    category

---
+++ BTL_Orcl/SqlFile/QLST_Category.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  File created - Saturday-October-26-2024   
+--------------------------------------------------------
+REM INSERTING into QLST.CATEGORIES
+SET DEFINE OFF;
+Insert into QLST.CATEGORIES (CATEGORYID,CATEGORYTITLE,DESCRIPTION) values ('C001','?? u?ng','Các lo?i n??c u?ng');
+Insert into QLST.CATEGORIES (CATEGORYID,CATEGORYTITLE,DESCRIPTION) values ('C002','Bánh k?o','Các lo?i bánh, k?o');
+Insert into QLST.CATEGORIES (CATEGORYID,CATEGORYTITLE,DESCRIPTION) values ('C003','S?a','Các s?n ph?m t? s?a');
+Insert into QLST.CATEGORIES (CATEGORYID,CATEGORYTITLE,DESCRIPTION) values ('C004','Rau c?','Rau c? t??i s?ng');
+Insert into QLST.CATEGORIES (CATEGORYID,CATEGORYTITLE,DESCRIPTION) values ('C005','Bánh mì','Các lo?i bánh mì, bánh ng?t');
+Insert into QLST.CATEGORIES (CATEGORYID,CATEGORYTITLE,DESCRIPTION) values ('C006','Gia v?','Các lo?i gia v? và ph? gia n?u ?n');
+Insert into QLST.CATEGORIES (CATEGORYID,CATEGORYTITLE,DESCRIPTION) values ('C007','Th?t và h?i s?n','Th?t t??i, cá, h?i s?n');
