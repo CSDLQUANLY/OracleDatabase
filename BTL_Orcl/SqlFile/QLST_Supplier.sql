4aaa5760140289993b67d9cb42e6bff5334565a8
quangnv04 <ngovanquang2601@gmail.com>
2024-10-26 21:11:15 +0700

    supplier

---
+++ BTL_Orcl/SqlFile/QLST_Supplier.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------
+--  File created - Saturday-October-26-2024   
+--------------------------------------------------------
+REM INSERTING into QLST.SUPPLIERS
+SET DEFINE OFF;
+Insert into QLST.SUPPLIERS (SUPPLIERID,SUPPLIERNAME,PHONENUMBER,EMAIL,ADDRESS,NOTE) values ('S001','Nhà cung c?p A','0123-456-789','nccA@gmail.com','123 ???ng A, TP.HN','Nhà cung c?p chính cho ?? u?ng');
+Insert into QLST.SUPPLIERS (SUPPLIERID,SUPPLIERNAME,PHONENUMBER,EMAIL,ADDRESS,NOTE) values ('S002','Nhà cung c?p B','0234-567-890','nccB@gmail.com','456 ???ng B, TP.DaLat','Nhà cung c?p chính cho bánh k?o');
+Insert into QLST.SUPPLIERS (SUPPLIERID,SUPPLIERNAME,PHONENUMBER,EMAIL,ADDRESS,NOTE) values ('S003','Nhà cung c?p C','0345-678-901','nccC@gmail.com','789 ???ng C, TP.BacNinh','Nhà cung c?p chính cho s?a');
+Insert into QLST.SUPPLIERS (SUPPLIERID,SUPPLIERNAME,PHONENUMBER,EMAIL,ADDRESS,NOTE) values ('S004','Nhà cung c?p D','0456-789-012','nccD@gmail.com','101 ???ng D, TP.HN','Nhà cung c?p chính cho rau c?');
+Insert into QLST.SUPPLIERS (SUPPLIERID,SUPPLIERNAME,PHONENUMBER,EMAIL,ADDRESS,NOTE) values ('S005','Nhà cung c?p E','0567-890-123','nccE@gmail.com','202 ???ng E, TP.BacNinh','Nhà cung c?p chính cho bánh mì');
+Insert into QLST.SUPPLIERS (SUPPLIERID,SUPPLIERNAME,PHONENUMBER,EMAIL,ADDRESS,NOTE) values ('S006','Nhà cung c?p F','0678-901-234','nccF@gmail.com','303 ???ng F, T.VinhPhuc','Nhà cung c?p gia v?');
+Insert into QLST.SUPPLIERS (SUPPLIERID,SUPPLIERNAME,PHONENUMBER,EMAIL,ADDRESS,NOTE) values ('S007','Nhà cung c?p G','0789-012-345','nccG@gmail.com','404 ???ng G, TP.QuangNinh','Nhà cung c?p th?t và h?i s?n');
+Insert into QLST.SUPPLIERS (SUPPLIERID,SUPPLIERNAME,PHONENUMBER,EMAIL,ADDRESS,NOTE) values ('S008','Nhà cung c?p H','0890-123-456','nccH@gmail.com','505 ???ng H, TP.HN','Nhà cung c?p rau c? và th?t t??i');
